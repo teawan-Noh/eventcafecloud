6ed49e4a34ca34288b2a4e4666af60396594fe63
nnakki <yvely225@gmail.com>
2022-06-30 00:46:31 +0900

    feat: User domain, repository 패키지 생성   - 유저 정보를 저장하기 위한 user   - 리프레시 토큰 정보를 저장하기 위한 userRefreshToken   - 일반 가입 시, 사용할 AuthReqModel 도메인   - 권한(role), 가입경로(providerType), 가입상태(status)는     각각 enum 클래스로 생성

---
+++ src/main/java/com/eventcafecloud/user/domain/AuthReqModel.java
@@ -0,0 +1,15 @@
+package com.eventcafecloud.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthReqModel {
+    private String userEmail;
+    private String userPassword;
+}
---
+++ src/main/java/com/eventcafecloud/user/domain/type/ProviderType.java
@@ -0,0 +1,9 @@
+package com.eventcafecloud.user.domain.type;
+
+import lombok.Getter;
+
+@Getter
+public enum ProviderType {
+    GOOGLE, NAVER, KAKAO, LOCAL;
+}
+
