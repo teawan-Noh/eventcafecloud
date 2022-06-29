4c0fbfdc7c9a3d0c6f6841592604ed25e0f68ff9
taewan <ntw2100@gmail.com>
2022-06-29 23:57:23 +0900

    [init] 패키지 구조 설정

---
+++ src/main/java/com/eventcafecloud/EventcafecloudApplication.java
@@ -0,0 +1,13 @@
+package com.eventcafecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventcafecloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventcafecloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventcafecloud/EventcafecloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventcafecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventcafecloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
