ab47f8de770a91bc440577c0b2bf3385ef1d58e6
yeonjue-2 <ilov-@naver.com>
2022-07-03 22:58:07 +0900

    [fix] EventReadResponseDto를 EventListResponseDtof로 바꾸고 새 EventReadResponseDto 생성

--- src/main/java/com/eventcafecloud/event/controller/EventController.java
+++ src/main/java/com/eventcafecloud/event/controller/EventController.java
@@ -4,30 +4,31 @@ import com.eventcafecloud.cafe.domain.Cafe;
 import com.eventcafecloud.event.dto.*;
 import com.eventcafecloud.event.service.EventService;
 import com.eventcafecloud.user.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class EventController {
 
     private final EventService eventService;
 
-    @Autowired
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
+    @GetMapping("/api/events")
+    public List<EventListResponseDto> getEvents() {
+        return eventService.getEvents();
     }
 
-    @GetMapping("/api/events")
-    public List<EventReadResponseDto> getEvents() {
-        return eventService.getEvents();
+    @PostMapping("/api/events")
+    public void createEvent(@RequestBody EventCreateRequestDto requestDto, User user, Cafe cafe) {
+        eventService.createEvent(requestDto, user, cafe);
     }
 
     @PutMapping("/api/events/{evnetNumber}")
 //    public EventUpdateResponseDto updateEvent(@PathVariable Long eventNumber, EventUpdateRequestDto requestDto){
-    public EventUpdateResponseDto updateEvent(@PathVariable Long eventNumber, EventUpdateRequestDto requestDto){
-        return eventService.updateEvent(eventNumber, requestDto);
+    public EventUpdateResponseDto updateEvent(@PathVariable Long id, EventUpdateRequestDto requestDto){
+        return eventService.updateEvent(id, requestDto);
     }
 
     @DeleteMapping("/api/events/{eventNumber}")
@@ -35,4 +36,9 @@ public class EventController {
         eventService.deleteEvent(eventNumber);
         return eventNumber;
     }
+
+//    @GetMapping("/api/events/{eventNumber}")
+//    public EventReadResponseDto getEvent(@PathVariable Long id) {
+//        return eventService.getEvent(id);
+//    }
 }
