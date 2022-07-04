ab4deae5c7f6db2caf3b41088b1b25c00f1ebf8f
yeonjue-2 <ilov-@naver.com>
2022-07-04 14:19:46 +0900

    [fix] updateEvent 수정, EventCreateRequestDto 에 카페, 유저 number 추가

--- src/main/java/com/eventcafecloud/event/dto/EventCreateRequestDto.java
+++ src/main/java/com/eventcafecloud/event/dto/EventCreateRequestDto.java
@@ -2,18 +2,13 @@ package com.eventcafecloud.event.dto;
 
 import com.eventcafecloud.event.domain.EventImage;
 import com.eventcafecloud.event.domain.type.EventCategory;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Getter
+@Data
 public class EventCreateRequestDto {
     private String eventName;
     private EventCategory eventCategory;
@@ -21,6 +16,7 @@ public class EventCreateRequestDto {
     private LocalDate eventEndDate;
     private String eventInfo;
     private int eventPrice;
-    private boolean eventCancelAvail;
     private List<MultipartFile> files;
+    private Long cafeNumber;
+    private Long userNumber;
 }
