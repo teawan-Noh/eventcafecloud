e20b5c74a7f2ef947bb7818b293f8a2f9c1beb6a
aichyu312 <aichyu312@gmail.com>
2022-07-04 21:51:06 +0900

    feat : 게시판 작성 / 리스트 화면단 연결

--- src/main/java/com/eventcafecloud/post/dto/PostReadResponseDto.java
+++ src/main/java/com/eventcafecloud/post/dto/PostReadResponseDto.java
@@ -1,15 +1,19 @@
 package com.eventcafecloud.post.dto;
 
+import com.eventcafecloud.common.base.BaseTimeEntity;
 import com.eventcafecloud.post.domain.type.PostType;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
-public class PostReadResponseDto {
+public class PostReadResponseDto extends BaseTimeEntity {
     private Long id;
     private String postTitle;
     private String postContent;
     private PostType postType;
     private Long postCount;
+    private LocalDateTime createdDate;
 }
