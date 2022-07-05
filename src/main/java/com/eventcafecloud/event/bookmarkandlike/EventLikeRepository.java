040c5c078f12a48e17a58bbf3796a698344baa5a
yeonjue-2 <ilov-@naver.com>
2022-07-05 20:06:01 +0900

    style: bookmark, like 관련 파일 정리

---
+++ src/main/java/com/eventcafecloud/event/bookmarkandlike/EventLikeRepository.java
@@ -0,0 +1,18 @@
+//package com.eventcafecloud.event.repository;
+//
+//import com.eventcafecloud.event.domain.EventLike;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//
+//import java.util.Optional;
+//
+//public interface EventLikeRepository extends JpaRepository<EventLike,Long> {
+//    long countByEventEventNumber(Long eventNumber);
+//
+//    @Query("SELECT el FROM EventLike el " +
+//            "JOIN User u " +
+//            "JOIN Event e " +
+//            "WHERE u.userEmail = :userEmail " +
+//            "AND e.eventNumber = :eventNumber")
+//    Optional<EventLike> findEventBookmarkByUserAndEvent(String userEmail, Long eventNumber);
+//}
