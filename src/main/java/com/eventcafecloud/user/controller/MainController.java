022027d7a45db21860fa4b2905b3ae3bc3fd9a76
nnakki <yvely225@gmail.com>
2022-07-05 17:47:46 +0900

    fix: 컨트롤러 수정 및 css,js 일부 추가

--- src/main/java/com/eventcafecloud/user/controller/MainController.java
+++ src/main/java/com/eventcafecloud/user/controller/MainController.java
@@ -1,23 +1,29 @@
 package com.eventcafecloud.user.controller;
 
 import com.eventcafecloud.oauth.token.AuthTokenProvider;
+import com.eventcafecloud.user.domain.User;
+import com.eventcafecloud.user.dto.HostUserCreateRequestDto;
+import com.eventcafecloud.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
     private final AuthTokenProvider tokenProvider;
+    private final UserService userService;
 
     @GetMapping("/")
-    public String main(@CookieValue(required = false, name = "access_token") String token, Model model){
-        if(token != null) {
+    public String main(@CookieValue(required = false, name = "access_token") String token, Model model) {
+        if (token != null) {
             String userEmail = tokenProvider.getUserEmailByToken(token);
-            model.addAttribute("userEmail", userEmail);
+            User user = userService.getUserByEmail(userEmail);
+            model.addAttribute("user", user);
         }
         return "index";
     }
@@ -25,5 +31,22 @@ public class MainController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/register/host")
+    public String registerHostForm(@CookieValue(required = false, name = "access_token") String token, Model model) {
+        String userEmail = tokenProvider.getUserEmailByToken(token);
+        model.addAttribute("hostUserCreateRequestDto", new HostUserCreateRequestDto());
+        model.addAttribute("Email", userEmail);
+        System.out.println(userEmail);
+        return "register/registerHostForm";
+    }
+
+    @PostMapping("/register")
+    public String saveHostUser(HostUserCreateRequestDto requestDto) {
+
+        userService.saveHostUser(requestDto);
+
+        return "redirect:/";
     }
 }
