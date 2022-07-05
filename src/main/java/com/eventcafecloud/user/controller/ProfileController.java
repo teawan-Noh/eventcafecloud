c369f9dfd73efae4999d4a932747b77b349144cc
nnakki <yvely225@gmail.com>
2022-07-05 17:43:03 +0900

    feat: 어드민페이지, 프로필페이지 컨트롤러 생성

---
+++ src/main/java/com/eventcafecloud/user/controller/ProfileController.java
@@ -0,0 +1,32 @@
+package com.eventcafecloud.user.controller;
+
+import com.eventcafecloud.user.dto.UserRequestDto;
+import com.eventcafecloud.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user/profile")
+public class ProfileController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}/info")
+    public String getUserProfileById(@PathVariable Long id, Model model) {
+        model.addAttribute("userRequestDto", new UserRequestDto());
+        model.addAttribute("user", userService.getUserById(id));
+        return "profile";
+    }
+
+    @PostMapping("/{id}/info/update")
+    public String updateUserProfile(@PathVariable Long id, UserRequestDto requestDto) {
+        userService.updateUserProfile(id, requestDto);
+        return "redirect:/user/profile/" + id + "/info";
+    }
+}
