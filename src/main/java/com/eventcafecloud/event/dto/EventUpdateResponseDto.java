777539f4b041450282f23cc7f8057c2aa7278129
yeonjue-2 <ilov-@naver.com>
2022-07-08 11:29:54 +0900

    fix: 자잘한 수정(exception 처리, 주석 달기)

--- src/main/java/com/eventcafecloud/event/dto/EventUpdateResponseDto.java
+++ src/main/java/com/eventcafecloud/event/dto/EventUpdateResponseDto.java
@@ -1,13 +1,8 @@
 package com.eventcafecloud.event.dto;
 
-import com.eventcafecloud.event.domain.EventImage;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
 @Setter
 public class EventUpdateResponseDto {
@@ -15,7 +10,7 @@ public class EventUpdateResponseDto {
     private String eventInfo;
     // private List<MultipartFile> files;
 
-    public EventUpdateResponseDto(String eventName, String eventInfo, List<MultipartFile> files){
+    public EventUpdateResponseDto(String eventName, String eventInfo){
         this.eventName = eventName;
         this.eventInfo = eventInfo;
         // this.files = files;
