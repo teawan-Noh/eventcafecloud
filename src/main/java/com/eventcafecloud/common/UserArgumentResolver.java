e60bdb3a7f81e63a5486c0e48f165f5211d95d65
nnakki <yvely225@gmail.com>
2022-07-08 20:23:17 +0900

    feat: UserArgumentResolver 등록   - userDetailService삭제(사이트 로그인X)   - 리졸버를 등록해주기 위해 WebConfig생성

---
+++ src/main/java/com/eventcafecloud/common/UserArgumentResolver.java
@@ -0,0 +1,30 @@
+package com.eventcafecloud.common;
+
+import com.eventcafecloud.oauth.domain.UserPrincipalForResolver;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+
+@Service
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return UserPrincipalForResolver.class.isAssignableFrom(methodParameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return authentication.getPrincipal();
+        }
+        return null;
+    }
+}
