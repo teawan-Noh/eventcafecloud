655108b7e0cc5c48482ff835f3bd37b9f45feb3b
taewan <ntw2100@gmail.com>
2022-07-10 21:57:28 +0900

    feat. CafeCreateRequestDto 제약조건 추가
    필수값 제약 조건 추가

--- src/main/java/com/eventcafecloud/cafe/dto/CafeCreateRequestDto.java
+++ src/main/java/com/eventcafecloud/cafe/dto/CafeCreateRequestDto.java
@@ -26,22 +26,29 @@ public class CafeCreateRequestDto {
     // 위도 : y : Latitude
     private Double cafeY;
 
+    @NotEmpty(message = "한 줄 소개 입력은 필수 입니다.")
     private String cafeInfo;
 
+    @NotEmpty(message = "상세 소개 입력은 필수 입니다.")
     private String cafeInfoDetail;
 
+    @NotEmpty(message = "주의 사항 입력은 필수 입니다.")
     private String cafePrecaution;
 
+    @NotEmpty(message = "평일 요금 입력은 필수 입니다.")
     private Integer cafeWeekdayPrice;
 
+    @NotEmpty(message = "주말 요금 입력은 필수 입니다.")
     private Integer cafeWeekendPrice;
 
     private String cafeOpenTime;
 
     private String cafeCloseTime;
 
+    @NotEmpty(message = "이미지 선택은 필수 입니다.")
     private List<MultipartFile> files;
 
+    @NotEmpty(message = "옵션 선택은 필수 입니다.")
     private List<CafeOptionType> options;
 
 }
