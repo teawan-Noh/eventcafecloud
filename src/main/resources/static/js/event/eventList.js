acc0cc0106d7f7c6d668329d380231e1b178a643
yeonjue-2 <ilov-@naver.com>
2022-07-10 23:49:14 +0900

    fix: 메인페이지 event top 5 css 수정

--- src/main/resources/static/js/event/eventList.js
+++ src/main/resources/static/js/event/eventList.js
@@ -22,24 +22,20 @@ function createHtml(event) {
     const eventStartDate = event["eventStartDate"];
     const eventEndDate = event["eventEndDate"];
 
-    return `<div class="card" onclick="#" style="width: 200px; height: 250px;">
+    return `<div class="card" onclick="#" >
                             <div class="card-image">
                                 <figure class="image is-4by3">
                                     <img src="${eventImageUrls[0]}" class="card-img-top" alt="Placeholder image">
                                 </figure>
                             </div>
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <h5><span class="badge badge-secondary">${eventCategory}</span></h5>
-                                        <p id="title" class="title is-4">${eventName}</p>
-                                        <div class="date-start">
-                                            <p id="event-start-date">${eventStartDate}</p>
-                                        </div>
-                                        <div class="date-end">
-                                            <p id="event-end-date"> ~ ${eventEndDate}</p>
-                                        </div>
-                                    </div>
+                            <div class="card-content" id="card-content">
+                                <h5><span class="badge badge-secondary">${eventCategory}</span></h5>
+                                <p id="title" class="title is-4">${eventName}</p>
+                                <div class="date-start">
+                                    <p id="event-start-date">${eventStartDate}</p>
+                                </div>
+                                <div class="date-end">
+                                    <p id="event-end-date"> ~ ${eventEndDate}</p>
                                 </div>
                             </div>
                        </div>`;
