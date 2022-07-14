ebe0027b78b3dadacd1dcd292709a0257708bf1c
nnakki <yvely225@gmail.com>
2022-07-14 12:20:02 +0900

    [WIP] feat: 호스트페이지 카페관리탭 수정

--- src/main/java/com/eventcafecloud/user/controller/hostProfileController.java
+++ src/main/java/com/eventcafecloud/user/controller/hostProfileController.java
@@ -2,6 +2,7 @@ package com.eventcafecloud.user.controller;
 
 import com.eventcafecloud.cafe.domain.Cafe;
 import com.eventcafecloud.cafe.domain.CafeSchedule;
+import com.eventcafecloud.cafe.dto.CafeScheduleRequestDto;
 import com.eventcafecloud.cafe.service.CafeScheduleService;
 import com.eventcafecloud.cafe.service.CafeService;
 import com.eventcafecloud.event.domain.Event;
@@ -15,10 +16,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -71,6 +75,14 @@ public class hostProfileController {
         model.addAttribute("userId", loginUser.getId());
         return "/profile-host/host-schedule";
     }
+
+    @PostMapping("/{id}/cafes/{cafeId}/schedule/holiday")
+    public String createHoliday(@Valid CafeScheduleRequestDto requestDto, BindingResult result, @PathVariable Long cafeId, User loginUser) {
+        cafeScheduleService.saveCafeSchedule(requestDto, cafeId);
+        
+        return "redirect:/hosts/profile/{id}/cafes/{cafeId}/schedule";
+    }
+
 
     @PostMapping("/{id}/info/update")
     public String updateUserProfile(@PathVariable Long id, UserRequestDto requestDto) {
