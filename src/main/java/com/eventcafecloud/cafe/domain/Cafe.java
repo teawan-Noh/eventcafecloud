1830721d9e8d064b41023d9e7314ca9bf6084542
taewan <ntw2100@gmail.com>
2022-07-17 21:32:45 +0900

    style. 변수 위치 변동되어 복원

--- src/main/java/com/eventcafecloud/cafe/domain/Cafe.java
+++ src/main/java/com/eventcafecloud/cafe/domain/Cafe.java
@@ -22,8 +22,9 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 다른곳에서 생성자 못쓰도록 막아둠
 public class Cafe extends BaseTimeEntity {
 
-    @OneToMany(mappedBy = "cafe", cascade = CascadeType.ALL)
-    private final List<CafeOption> cafeOptions = new ArrayList<>();
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cafe_number")
+    private Long id;
 
     @Column(nullable = false)
     private String cafeName;
@@ -56,7 +57,6 @@ public class Cafe extends BaseTimeEntity {
     private int cafeWeekendPrice;
 
     private String cafeOpenTime;
-
     private String cafeCloseTime;
 
     // 단반향 연관관계
@@ -65,20 +65,19 @@ public class Cafe extends BaseTimeEntity {
     private User user;
 
     @OneToMany(mappedBy = "cafe", cascade = CascadeType.ALL)
-    private final List<CafeImage> cafeImages = new ArrayList<>();
+    private List<CafeOption> cafeOptions = new ArrayList<>();
 
     @OneToMany(mappedBy = "cafe", cascade = CascadeType.ALL)
-    private final List<CafeReview> cafeReviews = new ArrayList<>();
+    private List<CafeImage> cafeImages = new ArrayList<>();
 
     @OneToMany(mappedBy = "cafe", cascade = CascadeType.ALL)
-    private final List<CafeSchedule> cafeSchedules = new ArrayList<>();
+    private List<CafeReview> cafeReviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "cafe", cascade = CascadeType.ALL)
+    private List<CafeSchedule> cafeSchedules = new ArrayList<>();
 
     @OneToMany(mappedBy = "cafe")
-    private final List<Event> events = new ArrayList<>();
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cafe_number")
-    private Long id;
+    private List<Event> events = new ArrayList<>();
 
     public Cafe(CafeCreateRequestDto requestDto) {
         this.cafeName = requestDto.getCafeName();
@@ -97,7 +96,7 @@ public class Cafe extends BaseTimeEntity {
     }
 
     //==연관관계 편의 메서드==//
-    public void addUser(User user) {
+    public void addUser(User user){
         this.user = user;
     }
 
