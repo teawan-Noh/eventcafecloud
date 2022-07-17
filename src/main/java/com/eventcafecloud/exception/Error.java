3a8494fff71fb15d9b502b03399f75b386b9b78f
taewan <ntw2100@gmail.com>
2022-07-17 21:11:51 +0900

    feat. 에러페이지 커스텀

---
+++ src/main/java/com/eventcafecloud/exception/Error.java
@@ -0,0 +1,35 @@
+package com.eventcafecloud.exception;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class Error implements ErrorController {
+    private String ERROR_TEMPLATES_PATH = "/error/";
+
+    @RequestMapping(value = "/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if(status != null){
+            int statusCode = Integer.valueOf(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                return ERROR_TEMPLATES_PATH + "404";
+            }
+            // 405
+            if(statusCode == HttpStatus.METHOD_NOT_ALLOWED.value()){
+                System.out.println("실행");
+                return ERROR_TEMPLATES_PATH + "405";
+            }
+
+            if(statusCode == HttpStatus.FORBIDDEN.value()){
+                return ERROR_TEMPLATES_PATH + "500";
+            }
+        }
+        return "error";
+    }
+}
