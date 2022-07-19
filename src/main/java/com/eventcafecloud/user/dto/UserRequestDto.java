c1c202e86fc5d9a096345f1e8127e38bfdef6d16
nnakki <yvely225@gmail.com>
2022-07-19 14:50:18 +0900

    fix: 로그인화면 포인터 변경 / 프로필 수정 유효성 검사 추가

--- src/main/java/com/eventcafecloud/user/dto/UserRequestDto.java
+++ src/main/java/com/eventcafecloud/user/dto/UserRequestDto.java
@@ -4,14 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 public class UserRequestDto {
     private Long userNumber;
     private String userEmail;
-    private String userPassword;
+    private String uerPassword;
+
+    @NotEmpty(message = "닉네임은 필수입니다.")
     private String userNickname;
     private String userGender;
+
+    @NotEmpty(message = "이미지 파일을 선택해 주세요.")
     private MultipartFile userImage;
     private String userRegPath;
     private String role;
