d866f381eb159cb8443ba41bfd141af00c4b173a
taewan <ntw2100@gmail.com>
2022-07-19 14:11:55 +0900

    fix. timezone test

--- src/main/java/com/eventcafecloud/EventcafecloudApplication.java
+++ src/main/java/com/eventcafecloud/EventcafecloudApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -15,6 +18,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 })
 public class EventcafecloudApplication {
 
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(EventcafecloudApplication.class, args);
     }
