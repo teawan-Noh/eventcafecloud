2514bd54816569a8b327c489b71dac8676d93853
taewan <ntw2100@gmail.com>
2022-07-21 13:20:22 +0900

    style: 주석 제거

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -1,8 +1,4 @@
 function logout() {
-    // $.removeCookie('access_token', {path: '/'});
-    // $.removeCookie('refresh_token', {path: '/'});
-    // alert('로그아웃 되었습니다.')
-    // window.location.href = '/';
     $.ajax({
         type: "GET",
         url: "/userLogout",
