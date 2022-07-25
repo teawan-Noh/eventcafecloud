d50ca5ea1e22d3af8998e65249fc59f937164f56
aichyu312 <aichyu312@gmail.com>
2022-07-26 00:49:17 +0900

    fix : 유저아이디가 다른 게시글수정 페이지 호출 및 수정 불가하게 변경

--- src/main/java/com/eventcafecloud/post/dto/PostUpdateRequestDto.java
+++ src/main/java/com/eventcafecloud/post/dto/PostUpdateRequestDto.java
@@ -1,20 +1,25 @@
 package com.eventcafecloud.post.dto;
 
 import com.eventcafecloud.post.domain.Post;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 public class PostUpdateRequestDto {
     @NotBlank(message = "내용을 입력해주세요.")
     private String postContent;
     private Long postId;
+    private Long userId;
 
     public static PostUpdateRequestDto toDto(Post post) {
         PostUpdateRequestDto requestDto = new PostUpdateRequestDto();
         requestDto.postContent = post.getPostContent();
         requestDto.postId = post.getId();
+        requestDto.userId = post.getUser().getId();
 
         return requestDto;
     }
