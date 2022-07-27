28c85274f764ae1b974d9bbbcee72ebffe880778
nnakki <yvely225@gmail.com>
2022-07-27 17:09:29 +0900

    style: 쓰지 않는 패키지 삭제

--- src/main/java/com/eventcafecloud/common/bookmark/BookmarkService.java
+++
@@ -1,7 +0,0 @@
-package com.eventcafecloud.common.bookmark;
-
-//public interface BookmarkService {
-//    BookmarkResponse createBookmark(Long eventNumber, String userEmail);
-//    BookmarkResponse findBookmark(Long eventNumber);
-//    BookmarkResponse deleteBookmark(Long eventNumber, String userEmail);
-//}
